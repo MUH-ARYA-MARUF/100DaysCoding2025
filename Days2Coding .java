2a588f248eae9ccc06033266a88de7e2d0a42977
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-09 16:40:40 +0800

    Create Days2Coding .java

---
+++ Days2Coding .java
@@ -0,0 +1,16 @@
+public class Days2 {
+	public static void main(String[] args) {
+        // println 
+        System.out.println("Penjual Bakso");
+        System.out.println("Nama: Asep");
+
+        // print
+        System.out.print("Menjual dari");
+        System.out.print(" pukul 07:30-12;00\n");
+
+        // printf 
+        String nama = "Asep";
+        int umur = 20;
+        System.out.printf("Nama: %s, Umur: %d tahun\n", nama, umur);
+	}
+}
