ae05e1bc606e06d79b38921fe57910d28e98b32b
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-10 13:38:53 +0800

    Create Coding days3 .java

---
+++ Coding days3 .java
@@ -0,0 +1,26 @@
+public class Days3 {
+	public static void main(String[] args) {
+		
+System.out.println("Komentar Baris Tunggal (Single-line comment)");
+ // Komentar setelah kode
+ 
+ 
+ 
+ /*
+   Ini adalah komentar
+   yang bisa mencakup
+   beberapa baris
+*/
+System.out.println("Banyak Baris (Muliti-line comment)");
+ 
+
+
+
+/**
+ * Method ini mencetak pesan ke layar.
+ * @param pesan Pesan yang akan dicetak
+ */
+    System.out.println("Komentar Dokumentasi(Javadoc Comment)");
+
+	}
+}
