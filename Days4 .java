df5a8a9b3a644ab6176a75509f0187d8049ec0ec
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-11 12:42:48 +0800

    Create Days4 .java

---
+++ Days4 .java
@@ -0,0 +1,8 @@
+public class Days4 {
+	public static void main(String[] args) {
+		
+        System.out.println("Nama:\tMuh Arya\nAlamat:\tPasangkayu");
+        System.out.println("Folder penyimpanan: C:\\Users\\Arya");
+        System.out.println("Kata: \"Semangat coding\"");
+	}
+}
