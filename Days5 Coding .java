05c2dc26824872d028c55a210f06e45561096aaf
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-12 20:32:33 +0800

    Create Days5 Coding .java

---
+++ Days5 Coding .java
@@ -0,0 +1,16 @@
+public class Days5 {
+	public static void main(String[] args) {
+		
+        byte umur = 18;
+        short tinggiBadan = 166;
+        int uangJajan = 20000;
+        long populasiDunia = 8000000000L;
+        
+        
+        System.out.println("Umur: " + umur);
+        System.out.println("tinggi Badan: " + tinggiBadan);
+        System.out.println("Populasi Dunia: " + populasiDunia);
+    
+ 
+	}
+}
