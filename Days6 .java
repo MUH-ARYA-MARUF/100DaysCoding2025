191626cae61df550f2ecfdb3a1815edced5f87bb
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-13 20:02:06 +0800

    Create Days6 .java

---
+++ Days6 .java
@@ -0,0 +1,14 @@
+
+public class Days6 {
+	public static void main(String[] args) {
+	       
+        double nilai1 = 82;
+        double nilai2 = 80;
+        double nilai3 = 90;
+
+        double rataRata = (nilai1 + nilai2 + nilai3) / 3;
+
+        System.out.println("Rata-rata nilai: " + rataRata);
+  
+	}
+}
