615591565591512b0ce5fcad4399f6b38be572be
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-15 19:34:14 +0800

    Create Days8 .java

---
+++ Days8 .java
@@ -0,0 +1,12 @@
+public class Days8 {
+	public static void main(String[] args) {
+		
+  String nama = "Asepp";
+  String salam = "Haiii, " + nama;
+
+   System.out.println(salam);
+   System.out.println("Banyak huruf nama: " + nama.length());
+   System.out.println("Huruf pertama: " + nama.charAt(0));
+ 
+	}
+}
