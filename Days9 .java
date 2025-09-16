932c03b5d0fb11f9de164c58d10f0ae45fb31214
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-16 19:56:08 +0800

    Create Days9 .java

---
+++ Days9 .java
@@ -0,0 +1,21 @@
+public class Days9 { 
+public static void main(String[] args) {
+		
+  // Deklarasi dan inisialisasi 
+        int angka = 80;
+        String nama = "Rival";
+
+  // Menampilkan nilai awal
+        System.out.println("Angka: " + angka);
+        System.out.println("Nama: " + nama);
+
+ // Update nilai variabel
+        angka = 90;
+        nama = "Dani";
+
+ // Menampilkan nilai setelah update
+        System.out.println("Angka baru: " + angka);
+        System.out.println("Nama baru: " + nama);
+   
+	}
+}
