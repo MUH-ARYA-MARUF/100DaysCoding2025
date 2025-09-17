d71cc0c317a0f436649796e2f321c24197fa11c8
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-17 20:59:40 +0800

    Create Days10 .java

---
+++ Days10 .java
@@ -0,0 +1,9 @@
+public class Aryaa {
+public static void main(String[] args) {
+		
+        final StringBuilder teks = new                StringBuilder("Salam");
+        teks.append(" Teknik");
+        System.out.println(teks);
+
+	}
+}
