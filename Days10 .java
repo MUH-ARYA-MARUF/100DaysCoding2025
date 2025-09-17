543eb238c89b1c90a3bcc6dbaa6aaf4698b444db
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-17 21:16:09 +0800

    Delete Days10 .java

--- Days10 .java
+++
@@ -1,9 +0,0 @@
-public class Aryaa {
-public static void main(String[] args) {
-		
-        final StringBuilder teks = new                StringBuilder("Salam");
-        teks.append(" Teknik");
-        System.out.println(teks);
-
-	}
-}
