56eff481f13f3e8a07fd23b100d44ba5ddab5676
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-20 22:31:59 +0800

    Create Days3 2 .java

---
+++ Days3 2 .java
@@ -0,0 +1,88 @@
+public class Main {
+	public static void main(String[] args) {
+		    int a1 = 11;
+        int a2 = 99;
+        long b1 = 55;
+        long b2 = 6665;
+        byte c1 = 77;
+        byte c2 = 87;
+        short d1 = 999;
+        short d2 = 998;
+        double e1 = 6.9;
+        double e2 = 6.3;
+        float f1 = 76.6f;
+        float f2 = 67.8f;
+        char g1 = 'p';
+        char g2 = 'o';
+        String h1 = "majene";
+        String h2 = "leppe";
+        boolean i1 = true;
+        boolean i2 = false;
+        
+       System.out.println("=====Variabel Sebelum Update=====");
+       
+       System.out.println(a1);
+       System.out.println(a2);
+       System.out.println(b1);
+       System.out.println(b2);
+       System.out.println(c1);
+       System.out.println(c2);
+       System.out.println(d1);
+       System.out.println(d2);
+       System.out.println(e1);
+       System.out.println(e2);
+       System.out.println(f1);
+       System.out.println(f2);
+       System.out.println(g1);
+       System.out.println(g2);
+       System.out.println(h1);
+       System.out.println(h2);
+       System.out.println(i1);
+       System.out.println(i2);
+       System.out.println();
+       
+       
+       
+       System.out.println("=====Variabel Sesudah Update=====");
+        int umur = 2;
+        int umur2 = 3;
+        long panjang = 12345;
+        long panjang2 = 67905;
+        byte luas = 23;
+        byte luas2 = 22;
+        short pendek = 789;
+        short pendek2 = 7789;
+        double koma = 2.9;
+        double koma2 = 6.9;
+        float yah = 28.9f;
+        float yah2 = 27.8f;
+        char huruf = 's';
+        char huruf2 = 'p';
+        String name = "olmo";
+        String nama2 = "dani";
+        boolean benar = true;
+        boolean salah = false;
+        
+        
+        
+        System.out.println(luas2);
+        System.out.println(luas);
+        System.out.println(pendek);
+        System.out.println(pendek2);
+        System.out.println(panjang);
+        System.out.println(panjang2);
+        System.out.println(umur);
+        System.out.println(umur2);
+        System.out.println(koma);
+        System.out.println(koma2);
+        System.out.println(yah);
+        System.out.println(yah2);
+        System.out.println(huruf);
+        System.out.println(huruf2);
+        System.out.println(name);
+        System.out.println(nama2);
+        System.out.println(benar);
+        System.out.println(salah);
+        System.out.println();
+	}
+}
