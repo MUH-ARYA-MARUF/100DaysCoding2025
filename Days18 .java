ec35d6ebfe102a22da486a7abc55132d1167fd3e
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-25 22:27:36 +0800

    Create Days18 .java

---
+++ Days18 .java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class aryaa {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+  
+        System.out.print("Masukkan angka (int): ");
+        int angka = input.nextInt();
+        double nilaiDouble = angka;
+       
+       
+        System.out.println("Nilai int    : " + angka);
+        System.out.println("Nilai double : " + nilaiDouble);
+ 
+	}
+}
