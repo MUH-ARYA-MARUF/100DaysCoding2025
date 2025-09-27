89f16d72e32a97c2082c3a5ee7f986448437ad0b
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-09-27 21:46:11 +0800

    Create Days20 .java

---
+++ Days20 .java
@@ -0,0 +1,15 @@
+	public class Main {
+    public static void main(String[] args) {
+        int umur = 19;
+        double bb = 70.4;
+        boolean status = true;
+
+        String strAngka = String.valueOf(umur);
+        String strBb = String.valueOf(bb);
+        String strStatus = String.valueOf(status);
+      
+        System.out.println("String dari int     : " + strAngka);
+        System.out.println("String dari double  : " + strBb);
+        System.out.println("String dari boolean : " + strStatus);
+    }
+}
