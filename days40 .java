28f7c19095e2cd1323e02e4c6497a3c7173d08a7
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2025-10-17 21:51:45 +0800

    Create days40 .java

---
+++ days40 .java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class KalkulatorIf {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input.nextDouble();
+
+        // Menggunakan if untuk menentukan operasi
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+            System.out.println("Hasil: " + hasil);
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+            System.out.println("Hasil: " + hasil);
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+            System.out.println("Hasil: " + hasil);
+        } else if (operator == '/') {
+            if (angka2 != 0) {
+                hasil = angka1 / angka2;
+                System.out.println("Hasil: " + hasil);
+            } else {
+                System.out.println("Error: Pembagian dengan nol tidak diperbolehkan.");
+            }
+        } else {
+            System.out.println("Operator tidak valid!");
+        }
+
+        input.close();
+    }
+}
